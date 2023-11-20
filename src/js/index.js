7dc0ef842cacc09146336a4d5a3515c88f5f4313
Alex <alexfedorko22@gmail.com>
2023-11-20 12:35:55 +0100

    Added Slider with photo gallery

--- src/js/index.js
+++ src/js/index.js
@@ -14,3 +14,37 @@ const modalBtn = $('#openModalLinkVideo');
 modalBtn.addEventListener('click', () => {
 
 })
+
+const btnBurgerMenu = document.getElementById('burger-btn');
+const mobMenu = document.querySelector('.menu');
+
+if (mobMenu) {
+    mobMenu.addEventListener('click', (e) => {
+        if (e.target.classList.contains('menu-link')) {
+            document.body.classList.remove('menu-active');
+        }
+    })
+}
+
+if (btnBurgerMenu) {
+    btnBurgerMenu.addEventListener('click', () => {
+        document.body.classList.toggle('menu-active');
+    })
+}
+
+window.addEventListener('load', () => {
+    new Swiper(".mySwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            type: "progressbar",
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+})
+
+
+
+
